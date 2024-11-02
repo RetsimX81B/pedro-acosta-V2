10437a93fb449893152b6d6a3dd05b05bda06aae
RetsimX81B <alexa.paquot@hotmail.com>
2024-11-02 18:51:57 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -7,4 +7,11 @@ bouton.addEventListener('click', () => {
         behavior: "smooth"
     })
 })
-      
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
